117d2a14cdef2b34df93a1a8eed2d93dafe89e8b
Mora <mlabisi98@gmail.com>
2022-10-06 09:40:58 -0700

    cleaning up ignite card component

--- app/components/ignite/Card.tsx
+++ app/components/ignite/Card.tsx
@@ -243,7 +243,7 @@ const $containerBase: ViewStyle = {
   borderRadius: spacing.medium,
   padding: spacing.extraSmall,
   borderWidth: 1,
-  shadowColor: colors.palette.neutral800,
+  shadowColor: colors.palette.greyscale["800"],
   shadowOffset: { width: 0, height: 12 },
   shadowOpacity: 0.08,
   shadowRadius: 12.81,
@@ -268,28 +268,28 @@ const $containerPresets = {
   default: [
     $containerBase,
     {
-      backgroundColor: colors.palette.neutral100,
-      borderColor: colors.palette.neutral300,
+      backgroundColor: colors.palette.greyscale["100"],
+      borderColor: colors.palette.greyscale["300"],
     },
   ] as StyleProp<ViewStyle>,
 
   reversed: [
     $containerBase,
-    { backgroundColor: colors.palette.neutral800, borderColor: colors.palette.neutral500 },
+    { backgroundColor: colors.palette.greyscale["800"], borderColor: colors.palette.greyscale["500"] },
   ] as StyleProp<ViewStyle>,
 }
 
 const $headingPresets: Record<Presets, TextStyle> = {
   default: {},
-  reversed: { color: colors.palette.neutral100 },
+  reversed: { color: colors.palette.greyscale["100"] },
 }
 
 const $contentPresets: Record<Presets, TextStyle> = {
   default: {},
-  reversed: { color: colors.palette.neutral100 },
+  reversed: { color: colors.palette.greyscale["100"] },
 }
 
 const $footerPresets: Record<Presets, TextStyle> = {
   default: {},
-  reversed: { color: colors.palette.neutral100 },
+  reversed: { color: colors.palette.greyscale["100"] },
 }
